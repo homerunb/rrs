d42aade0e5f25b1f3ee719e943bd6cd99c1f199f
homerunb <freeminjh@naver.com>
2024-08-27 17:45:36 +0900

    맛집 관리 API 스펙 추가

--- review/src/main/java/com/jyujyu/review/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jyujyu.review.repository;
-
-import com.jyujyu.review.model.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
